55d6911c463887bc1bd6c9456e7f70abd73a98de
yi-yang-sipaway <yychcsg@gmail.com>
2024-07-11 17:26:26 -0700

    Initial Commit

---
+++ project_tt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  project_tt
+//
+//  Created by Yi Y on 7/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ project_tt/project_ttApp.swift
@@ -0,0 +1,17 @@
+//
+//  project_ttApp.swift
+//  project_tt
+//
+//  Created by Yi Y on 7/11/24.
+//
+
+import SwiftUI
+
+@main
+struct project_ttApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
